069489ee39925a01a2df6380da54056a386ab2ae
Nacycher <nacycher@163.com>
2022-07-26 20:42:00 +0800

    add study demo

---
+++ src/com/bolin/lock/DemoLock01.java
@@ -0,0 +1,73 @@
+package com.bolin.lock;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+// 创建资源类，定变量和操作资源的方法
+class Number {
+    private int number = 0;
+
+    Lock lock = new ReentrantLock();
+    Condition condition = lock.newCondition();
+
+    public void addNumber() throws InterruptedException {
+        lock.lock();
+        while (number != 0) {
+            condition.await();
+        }
+        try {
+            ++number;
+            System.out.println(Thread.currentThread().getName() + " : " + number);
+        condition.signalAll();
+        } finally {
+            lock.unlock();
+        }
+
+    }
+
+    public void reduceNumber() throws InterruptedException {
+        lock.lock();
+
+        while (number != 1) {
+            condition.await();
+        }
+
+        try {
+            --number;
+            System.out.println(Thread.currentThread().getName() + " : " + number);
+            condition.signalAll();
+        } finally {
+            lock.unlock();
+        }
+
+    }
+
+}
+
+public class DemoLock01 {
+    public static void main(String[] args) {
+        Number number = new Number();
+        new Thread(() -> {
+            try {
+                for (int i = 0; i < 10; i++) {
+                    number.addNumber();
+                }
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }, "A").start();
+
+        new Thread(() -> {
+            try {
+                for (int i = 0; i < 10; i++) {
+                    number.reduceNumber();
+                }
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }, "B").start();
+
+
+    }
+}
---
+++ src/com/bolin/sync/DemoSync01.java
@@ -0,0 +1,78 @@
+package com.bolin.sync;
+
+// 创建资源类，定义变量和操作资源类方法
+class Number {
+    private int number = 0;
+
+    public synchronized void addNumer() throws InterruptedException {
+        // 1.线程wait条件
+//            使用while避免虚假唤醒
+//            if (number != 0) {
+        while (number != 0) {
+            this.wait();
+        }
+        // 2.操作资源、
+        ++number;
+        System.out.println(Thread.currentThread().getName() + " : " + number);
+        // 3.唤醒其他线程
+        this.notifyAll();
+    }
+
+    public synchronized void reduceNumer() throws InterruptedException {
+        // 1.线程wait条件
+//            if (number != 1) {
+        while (number != 1) {
+            this.wait();
+        }
+        // 2.操作资源、
+        --number;
+        System.out.println(Thread.currentThread().getName() + " : " + number);
+        // 3.唤醒其他线程
+        this.notifyAll();
+    }
+}
+
+public class DemoSync01 {
+    public static void main(String[] args) {
+        Number number = new Number();
+        new Thread(() -> {
+            try {
+                for (int i = 0; i < 10; i++) {
+                    number.addNumer();
+                }
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }, "A").start();
+
+        new Thread(() -> {
+            try {
+                for (int i = 0; i < 10; i++) {
+                    number.addNumer();
+                }
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }, "B").start();
+
+        new Thread(() -> {
+            try {
+                for (int i = 0; i < 10; i++) {
+                    number.reduceNumer();
+                }
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }, "C").start();
+
+        new Thread(() -> {
+            try {
+                for (int i = 0; i < 10; i++) {
+                    number.reduceNumer();
+                }
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }, "D").start();
+    }
+}
