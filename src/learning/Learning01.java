0c03d6c15bb066abc21661ee7ed6e951c39fc78b
nacycher <nacycher@163.com>
2024-07-08 12:04:06 +0800

    change lambda expression

--- src/learning/Learning01.java
+++ src/learning/Learning01.java
@@ -2,13 +2,14 @@ package learning;
 
 public class Learning01 {
     public static void main(String[] args) {
-        Thread a = new Thread(() -> {
-            System.out.println(Thread.currentThread().getName() + " : " + Thread.currentThread().isDaemon());
-        }, "A");
-
+        Thread a = new Thread(Learning01::run, "A");
         // 设置守护线程
         // 当JVM中只有守护线程，没有用户线程时，JVM会停止。
 
         a.setDaemon(true);
+    }
+
+    private static void run() {
+        System.out.println(Thread.currentThread().getName() + " : " + Thread.currentThread().isDaemon());
     }
 }
